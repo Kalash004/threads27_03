294061569d95400c75fa02110a635ffb35eaf977
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2023-03-27 07:49:10 +0200

    start

---
+++ threads27_03/IObservable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads27_03
+{
+    enum EventType
+    {
+        Damaged,
+        Dead
+    }
+    internal interface IObservable
+    {
+        public void DamageHappened(EventType type, int damage);   
+    }
+}
---
+++ threads27_03/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads27_03
+{
+    internal class Player
+    {
+        private String name;
+        private int health;
+        private List<IObservable> observers;
+
+        public Player(string name, int health)
+        {
+            this.name = name;
+            this.health = health;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Health { get => health; set => health = value; }
+        
+        public void GetDamaged(int amount)
+        {
+            if (observers.Count > 0)
+            {
+                Thread event_notif = new Thread(new ParameterizedThreadStart(() => NotifyDamage(amount)));
+            }
+        }
+
+        public void BecomeObserver(IObservable obj)
+        {
+            this.observers.Add(obj);
+        }
+
+        private void NotifyDamage(int damage)
+        {
+
+        }
+    }
+}
