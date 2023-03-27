9ef453fa0a34140a2f9d106a44dc6db56afc7bde
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2023-03-27 09:08:53 +0200

    2nd not done

--- threads27_03/Program.cs
+++ threads27_03/Program.cs
@@ -1,28 +1,65 @@
 ﻿namespace threads27_03
 {
-    internal class Program:IObservable
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Player player = new Player("Kal",1000);
-            Thread damager = new Thread(() => Damage(player,10));
-            Thread healer = new Thread(() => Heal(player,10));
+            Player player = new Player("Kal", 1000);
+            Thread damager = new Thread(() => Damage(player, 10));
+            Thread healer = new Thread(() => Heal(player, 10));
             Reader reader = new Reader(player);
             damager.Start();
             healer.Start();
+
+            string path = "dluznici.csv";
+            List<String[]> list = ArrayIt(path);
+            int[] ids = GetIds(list);
+            ThreadedQuickSort<int> sort = new ThreadedQuickSort<int>();
+            sort.QuickSort(ids);
+
+        }
+
+        private static int[] GetIds(List<String[]> list)
+        {
+            int [] ids = new int[list.Count];
+            int i = 0;
+            foreach (var obj in list)
+            {
+                ids[i] = int.Parse(obj[0]);
+                i++;
+            }
+            return ids;
+        }
+
+        public static void Find(int id, string path)
+        {
+        }
+
+        public static List<String[]> ArrayIt(string path)
+        {
+            List<String[]> list = new List<string[]>();
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                list.Add(columns);
+            }
+            return list;
         }
 
         public static void Damage(Player plr, int testamount)
         {
-            for (int i = 0; i < testamount; i++) { 
-                plr.GetDamaged(RandomAmount(10,50));
+            for (int i = 0; i < testamount; i++)
+            {
+                plr.GetDamaged(RandomAmount(10, 50));
             }
         }
 
         public static void Heal(Player plr, int testamount)
         {
-            for (int i = 0; i < testamount; i++) {
-                plr.GetHealed(RandomAmount(10,50));
+            for (int i = 0; i < testamount; i++)
+            {
+                plr.GetHealed(RandomAmount(10, 50));
             }
         }
 
@@ -31,14 +68,6 @@
             return new Random().Next(low, max);
         }
 
-        public void DamageHappened(EventType type, int damage, int hp_left)
-        {
-            Console.WriteLine($"Damaged : {damage} Hp left : {hp_left}");
-        }
 
-        public void HealHappened(EventType type, int heal_amount, int hp_left)
-        {
-            Console.WriteLine($"Healed : {heal_amount} Hp left : {hp_left}");
-        }
     }
 }
---
+++ threads27_03/ThreadedQuickSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threads27_03
+{
+	public class ThreadedQuickSort<T> where T : IComparable<T>
+	{
+		public async Task QuickSort(T[] arr)
+		{
+			await QuickSort(arr, 0, arr.Length - 1);
+		}
+
+		private async Task QuickSort(T[] arr, int left, int right)
+		{
+
+			if (right <= left) return;
+			int lt = left;
+			int gt = right;
+			var pivot = arr[left];
+			int i = left + 1;
+			while (i <= gt)
+			{
+				int cmp = arr[i].CompareTo(pivot);
+				if (cmp < 0)
+					Swap(arr, lt++, i++);
+				else if (cmp > 0)
+					Swap(arr, i, gt--);
+				else
+					i++;
+			}
+
+			var t1 = Task.Run(() => QuickSort(arr, left, lt - 1));
+			var t2 = Task.Run(() => QuickSort(arr, gt + 1, right));
+
+			await Task.WhenAll(t1, t2).ConfigureAwait(false);
+
+		}
+		private void Swap(T[] a, int i, int j)
+		{
+			var swap = a[i];
+			a[i] = a[j];
+			a[j] = swap;
+		}
+    }
+}
